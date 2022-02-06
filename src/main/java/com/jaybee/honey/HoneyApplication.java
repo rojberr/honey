ae2cf33482da42fa872e33ac0ed0eb7cc1e4ee0d
Rojberr <rojberr@outlook.com>
2022-02-06 16:59:12 +0100

    feat(main): Make search run on start

--- src/main/java/com/jaybee/honey/HoneyApplication.java
+++ src/main/java/com/jaybee/honey/HoneyApplication.java
@@ -1,13 +1,28 @@
 package com.jaybee.honey;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class HoneyApplication {
+public class HoneyApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HoneyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyApplication.class, args);
+    }
 
+    private final CatalogService catalogService;
+
+    public HoneyApplication(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        List<Honey> honeyList = catalogService.findByName("Small");
+        honeyList.forEach(System.out::println);
+    }
 }
