81ee0d7b16147f4996015112dec04dcfd2e13719
Rojberr <rojberr@outlook.com>
2022-02-11 08:51:51 +0100

    feat(catalog-web): Add /catalog REST point and spring-boot-web dependency

---
+++ src/main/java/com/jaybee/honey/catalog/web/CatalogController.java
@@ -0,0 +1,25 @@
+package com.jaybee.honey.catalog.web;
+
+import com.jaybee.honey.catalog.application.port.CatalogUseCase;
+import com.jaybee.honey.catalog.domain.Honey;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping(value = "/catalog")
+@RestController
+@AllArgsConstructor
+public class CatalogController {
+
+    private final CatalogUseCase catalog;
+
+
+    @GetMapping
+    public List<Honey> getAll() {
+
+        return catalog.findAll();
+    }
+}
