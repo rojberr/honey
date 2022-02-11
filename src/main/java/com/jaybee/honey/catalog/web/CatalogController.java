02a76d372709b8c12790d54ebffde7f3757fd60b
Rojberr <rojberr@outlook.com>
2022-02-11 19:43:44 +0100

    feat(catalog:web): Add 404 http response for getById not found

--- src/main/java/com/jaybee/honey/catalog/web/CatalogController.java
+++ src/main/java/com/jaybee/honey/catalog/web/CatalogController.java
@@ -3,10 +3,9 @@ package com.jaybee.honey.catalog.web;
 import com.jaybee.honey.catalog.application.port.CatalogUseCase;
 import com.jaybee.honey.catalog.domain.Honey;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +18,18 @@ public class CatalogController {
 
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<Honey> getAll() {
 
         return catalog.findAll();
     }
 
     @GetMapping(value = "/{id}")
-    public Honey getById(@PathVariable Long id) {
+    public ResponseEntity<?> getById(@PathVariable Long id) {
 
-        return catalog.findById(id).orElse(null);
+        return catalog
+                .findById(id)
+                .map(ResponseEntity :: ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
