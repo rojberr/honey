5bb14feee1f563e487c565ebcaf35829a2db1afe
Rojberr <rojberr@outlook.com>
2022-02-18 21:08:38 +0100

    feat(order:domain): Add modifiedAt Date for Orders in database

--- src/main/java/com/jaybee/honey/order/domain/Order.java
+++ src/main/java/com/jaybee/honey/order/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -39,4 +40,6 @@ public class Order {
     @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
