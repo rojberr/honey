fa8c69d5c1c110f46238e7aa687dd2bb79a9c4b6
Rojberr <rojberr@outlook.com>
2022-03-09 19:49:44 +0100

    test(catalog): Test getAllBooks using http web request MockMvc

--- src/main/java/com/jaybee/honey/HoneyApplication.java
+++ src/main/java/com/jaybee/honey/HoneyApplication.java
@@ -7,13 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableScheduling
-@EnableJpaAuditing
 @SpringBootApplication
 @EnableSwagger2
 @EnableConfigurationProperties(OrderProperties.class)
---
+++ src/test/java/com/jaybee/honey/catalog/web/CatalogControllerWebTest.java
@@ -0,0 +1,43 @@
+package com.jaybee.honey.catalog.web;
+
+import com.jaybee.honey.catalog.application.port.CatalogUseCase;
+import com.jaybee.honey.catalog.domain.Honey;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({CatalogController.class})
+class CatalogControllerWebTest {
+
+    @MockBean
+    CatalogUseCase catalogUseCase;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void shouldGetAllBooks() throws Exception {
+        // Given
+        Honey honey1 = new Honey("Name 1", BigDecimal.valueOf(25), 99, 5L);
+        Honey honey2 = new Honey("Name 2", BigDecimal.valueOf(50), 1234, 5L);
+        Mockito.when(catalogUseCase.findAll()).thenReturn(List.of(honey1, honey2));
+
+        // Expect
+        mockMvc.perform(get("/catalog"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+}
