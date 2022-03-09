f33a7cfa047cc5c208ebee121a3d04122ae302d6
Rojberr <rojberr@outlook.com>
2022-03-09 17:42:16 +0100

    test(order): Test userCantOrderMoreBooksThanAvailable with exception thrown

--- src/test/java/com/jaybee/honey/order/application/ManipulateOrderServiceTest.java
+++ src/test/java/com/jaybee/honey/order/application/ManipulateOrderServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Import;
 import java.math.BigDecimal;
 
 import static com.jaybee.honey.order.application.port.ManipulateOrderUseCase.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
@@ -26,8 +27,8 @@ class ManipulateOrderServiceTest {
     @Test
     public void userCanPlaceOrder() {
         // Given
-        Honey honey1 = givenHoney1();
-        Honey honey2 = givenHoney2();
+        Honey honey1 = givenHoney1(88L);
+        Honey honey2 = givenHoney2(44L);
         PlaceOrderCommand command = PlaceOrderCommand
                 .builder()
                 .recipient(recipient())
@@ -42,15 +43,34 @@ class ManipulateOrderServiceTest {
         assertTrue(response.isSuccess());
     }
 
-    private Honey givenHoney1() {
-        return honeyRepository.save(new Honey("Name 1", BigDecimal.valueOf(25), 99, 88L));
+    private Honey givenHoney1(long available) {
+        return honeyRepository.save(new Honey("Name 1", BigDecimal.valueOf(25), 99, available));
     }
 
-    private Honey givenHoney2() {
-        return honeyRepository.save(new Honey("Name 2", BigDecimal.valueOf(50), 1234, 44L));
+    private Honey givenHoney2(long available) {
+        return honeyRepository.save(new Honey("Name 2", BigDecimal.valueOf(50), 1234, available));
     }
 
     private Recipient recipient() {
         return Recipient.builder().email("client-email@email.com").build();
+    }
+
+    @Test
+    public void userCantOrderMoreBooksThanAvailable() {
+        // Given
+        Honey honey1 = givenHoney1(5L);
+        PlaceOrderCommand command = PlaceOrderCommand
+                .builder()
+                .recipient(recipient())
+                .item(new OrderItemCommand(honey1.getId(), 10))
+                .build();
+
+        // When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.placeOrder(command);
+        });
+
+        // Then
+        assertTrue(exception.getMessage().contains("Too many products with id " + honey1.getId() + " requested: 10 available: 5 !"));
     }
 }
